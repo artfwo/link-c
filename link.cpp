29c7d91a19433c536e620cd56937ac7c47629128
Artem Popov <artfwo@gmail.com>
2019-04-05 20:31:12 +0200

    Initial bindings

---
+++ link.cpp
@@ -0,0 +1,70 @@
+#include "ableton/Link.hpp"
+#include "link.h"
+
+AbletonLink *
+ableton_link_new(double bpm)
+{
+    ableton::Link * link = NULL;
+    link = new ableton::Link(bpm);
+    return reinterpret_cast<AbletonLink *>(link);
+}
+
+void
+ableton_link_enable(AbletonLink *link_ptr, bool bEnable)
+{
+    ableton::Link *link = reinterpret_cast<ableton::Link*>(link_ptr);
+    link->enable(bEnable);
+}
+
+AbletonLinkClock *
+ableton_link_clock(AbletonLink *link_ptr)
+{
+    ableton::Link *link = reinterpret_cast<ableton::Link*>(link_ptr);
+    ableton::Link::Clock clock = link->clock();
+    ableton::Link::Clock *clock_ptr = new ableton::Link::Clock(clock);
+    return reinterpret_cast<AbletonLinkClock*>(clock_ptr);
+}
+
+AbletonLinkSessionState *
+ableton_link_capture_app_session_state(AbletonLink *link_ptr)
+{
+    ableton::Link *link = reinterpret_cast<ableton::Link*>(link_ptr);
+    ableton::Link::SessionState state = link->captureAppSessionState();
+    ableton::Link::SessionState *state_ptr = new ableton::Link::SessionState(state);
+    return reinterpret_cast<AbletonLinkSessionState*>(state_ptr);
+}
+
+void
+ableton_link_destroy(AbletonLink *link_ptr)
+{
+    ableton::Link *link = reinterpret_cast<ableton::Link*>(link_ptr);
+    delete link;
+}
+
+long
+ableton_link_clock_micros(AbletonLinkClock *clock_ptr)
+{
+    ableton::Link::Clock *clock = reinterpret_cast<ableton::Link::Clock*>(clock_ptr);
+    return clock->micros().count();
+}
+
+double
+ableton_link_session_state_tempo(AbletonLinkSessionState *state_ptr)
+{
+    ableton::Link::SessionState *state = reinterpret_cast<ableton::Link::SessionState*>(state_ptr);
+    return state->tempo();
+}
+
+double
+ableton_link_session_state_beat_at_time(AbletonLinkSessionState *state_ptr, long time, double quantum)
+{
+    ableton::Link::SessionState *state = reinterpret_cast<ableton::Link::SessionState*>(state_ptr);
+    return state->beatAtTime(std::chrono::microseconds(time), quantum);
+}
+
+void
+ableton_link_session_state_destroy(AbletonLinkSessionState *state_ptr)
+{
+    ableton::Link::SessionState *state = reinterpret_cast<ableton::Link::SessionState*>(state_ptr);
+    delete state;
+}
---
+++ link.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct AbletonLink;
+struct AbletonLinkClock;
+struct AbletonLinkSessionState;
+
+AbletonLink *ableton_link_new(double bpm);
+void ableton_link_enable(AbletonLink *link_ptr, bool bEnable);
+AbletonLinkClock *ableton_link_clock(AbletonLink *link_ptr);
+AbletonLinkSessionState *ableton_link_capture_app_session_state(AbletonLink *link);
+void ableton_link_destroy(AbletonLink *link);
+
+long ableton_link_clock_micros(AbletonLinkClock *clock_ptr);
+
+double ableton_link_session_state_tempo(AbletonLinkSessionState *state);
+double ableton_link_session_state_beat_at_time(AbletonLinkSessionState *state_ptr, long time, double quantum);
+void ableton_link_session_state_destroy(AbletonLinkSessionState *state);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
